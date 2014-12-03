cb94debc423e51e1fbeb302e121230c9393e4c36
GoesToEleven <todd.mcleod@fresnocitycollege.edu>
2014-12-02 16:13:17 -0800

    adds through chp 8 layouts crim intent

---
+++ CriminalIntent/app/src/main/java/com/example/toddmcleod/criminalintent/Crime.java
@@ -0,0 +1,47 @@
+package com.example.toddmcleod.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by toddmcleod on 12/2/14.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+}
---
+++ CriminalIntent/app/src/main/java/com/example/toddmcleod/criminalintent/CrimeFragment.java
@@ -0,0 +1,70 @@
+package com.example.toddmcleod.criminalintent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * Created by toddmcleod on 12/2/14.
+ */
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, MMM F, y", Locale.US);
+        mDateButton.setText(sdf.format(mCrime.getDate()));
+//        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        return v;
+    }
+}
