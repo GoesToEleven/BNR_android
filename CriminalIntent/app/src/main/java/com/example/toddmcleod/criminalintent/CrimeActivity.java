6da418c9cb8807ec84741caf028bcf88c69dbcb3
GoesToEleven <todd.mcleod@fresnocitycollege.edu>
2014-12-02 16:13:35 -0800

    adds through chp 8 layouts crim intent #2

---
+++ CriminalIntent/app/src/main/java/com/example/toddmcleod/criminalintent/CrimeActivity.java
@@ -0,0 +1,40 @@
+package com.example.toddmcleod.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.MenuItem;
+
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
